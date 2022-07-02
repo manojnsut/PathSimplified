92c22577bc9001d447910518591c3cf8825504ff
Rohit Kumar Rajak <rohit672kumar@gmail.com>
2022-07-02 17:40:35 +0530

    local server

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathFind from './components/PathFind';
+
+function App() {
+  return (
+    <div>
+        <PathFind />
+    </div>
+  );
+}
+
+export default App;
